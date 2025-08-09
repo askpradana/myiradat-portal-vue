6e1e84e3857462dc123772e1487ed53d588bee4f
rizalyoga <ach.rizal.yogaswara@gmail.com>
2025-08-09 23:18:15 +0700

    feat: post service user

---
+++ src/api/services/postUserService.ts
@@ -0,0 +1,76 @@
+import { useUserStore } from '@/stores/userStores'
+import { refreshToken } from '../refreshToken'
+
+export interface DataServiceProps {
+  service_code: string[]
+}
+
+export interface postUserServicAPIResponse {
+  success: boolean
+  message: string
+  data: {
+    failed: string[]
+    linked: string[]
+  }
+  timestamp: string
+}
+
+export const postUserServiceData = async (
+  newServiceData: DataServiceProps,
+  userID?: string,
+): Promise<postUserServicAPIResponse> => {
+  try {
+    const userStore = useUserStore()
+    const token = userStore.auth?.token
+
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/admin/users/${userID}/services`, {
+      method: 'POST',
+      body: JSON.stringify(newServiceData),
+      headers: {
+        Authorization: `bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+
+    if (!response.ok) {
+      const errorData = await response.json()
+      const errorMessage = errorData.message || 'An error occurs on the server'
+
+      if (response.status === 404) {
+        throw new Error(errorMessage)
+      } else if (response.status === 401) {
+        try {
+          const refreshResponse = await refreshToken()
+          if (userStore.auth && refreshResponse.data) {
+            userStore.auth.token = refreshResponse.data.token
+            userStore.auth.expires_at = refreshResponse.data.expires_at
+
+            const auth = {
+              token: refreshResponse.data.token,
+              expires_at: refreshResponse.data.expires_at,
+            }
+            sessionStorage.setItem('auth_token', JSON.stringify(auth))
+
+            return await postUserServiceData(newServiceData, userID)
+          } else {
+            throw new Error(errorMessage || 'The session has ended, please login again')
+          }
+        } catch (error) {
+          console.error('Token refresh error:', error)
+          throw new Error(errorMessage || 'Failed to update the token')
+        }
+      } else if (response.status === 400) {
+        throw new Error(errorMessage)
+      } else {
+        throw new Error(errorMessage)
+      }
+    }
+
+    const data = await response.json()
+
+    return data
+  } catch (error) {
+    console.error('Error:', error)
+    throw error
+  }
+}
--- src/views/admin/UserServicesListView.vue
+++ src/views/admin/UserServicesListView.vue
@@ -1,10 +1,61 @@
 <template>
   <DashboardLayout>
     <Button @click="backToDashboard" variant="default"> <ArrowLeft /> Back to dashboard </Button>
+
+    <h2 class="my-8 font-bold text-xl">Admin Services</h2>
+    <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+      <Card v-for="service in userStore.services" :key="service.code" class="w-full">
+        <CardHeader>
+          <CardTitle class="text-xs font-bold text-foreground">{{ service.name }}</CardTitle>
+          <CardContent class="px-0">
+            <span class="flex items-center gap-4">
+              <p class="text-xs">{{ service.code }}</p>
+              <Copy
+                :size="14"
+                class="text-primary cursor-pointer hover:text-primary/80"
+                @click="copyCodeService(service.code)"
+              />
+            </span>
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </div>
+
+    <span class="flex items-end gap-2">
+      <TagsInputRoot
+        v-model="modelValue"
+        class="flex gap-2 items-center border p-1.5 rounded-lg w-full max-w-[340px] flex-wrap bg-white shadow-sm mt-8"
+      >
+        <TagsInputItem
+          v-for="item in modelValue"
+          :key="item"
+          :value="item"
+          class="text-white bg-violet-500 font-medium flex items-center justify-center gap-2 bg-grass8 aria-[current=true]:bg-grass9 rounded p-0.5"
+        >
+          <TagsInputItemText class="text-xs pl-1" />
+          <TagsInputItemDelete class="p-0.5 rounded bg-transparent hover:bg-blackA4">
+            <X :size="10" />
+          </TagsInputItemDelete>
+        </TagsInputItem>
+
+        <TagsInputInput
+          placeholder="Service code..."
+          class="text-sm focus:outline-none flex-1 rounded text-grass9 bg-transparent placeholder:text-mauve9 px-1"
+        />
+      </TagsInputRoot>
+      <Button
+        size="sm"
+        @click="onSubmit"
+        :disabled="isSubmitService"
+        :class="isSubmitService && 'bg-gray-500 pointer-events-none'"
+        >{{ isSubmitService ? 'Please wait...' : 'Submit' }}</Button
+      >
+    </span>
+
     <h2 class="my-8 font-bold text-xl">User Services</h2>
 
     <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-      <template v-if="isPending">
+      <template v-if="isetUserServiveLoading">
         <CardServiceSkeleton />
       </template>
 
@@ -35,18 +86,32 @@ import Button from '@/components/ui/button/Button.vue'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { ArrowLeft } from 'lucide-vue-next'
 import { useRouter, useRoute } from 'vue-router'
-import { useQuery } from '@tanstack/vue-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query'
+import { useUserStore } from '@/stores/userStores'
 import { getUserService, type UserServiceResponse } from '@/api/services/getUserServices'
 import type { Ref } from 'vue'
 import UnlinkServiceAlert from '@/components/custom/alerts/UnlinkServiceAlert.vue'
 import CardServiceSkeleton from '@/components/custom/skeletons/CardServiceSkeleton.vue'
+import { X, Copy } from 'lucide-vue-next'
+import {
+  TagsInputInput,
+  TagsInputItem,
+  TagsInputItemDelete,
+  TagsInputItemText,
+  TagsInputRoot,
+} from 'reka-ui'
+import { ref } from 'vue'
+import { toast } from 'vue-sonner'
+import { postUserServiceData } from '@/api/services/postUserService'
 
 const router = useRouter()
 const route = useRoute()
+const userStore = useUserStore()
 
 const userID = route.params.id
+const modelValue = ref<string[]>([])
 
-const { isPending, data } = useQuery({
+const { isPending: isetUserServiveLoading, data } = useQuery({
   queryKey: ['services', userID],
   queryFn: () => getUserService(userID as string),
   staleTime: 1000 * 60 * 2, // 5 minutes
@@ -57,5 +122,45 @@ const { isPending, data } = useQuery({
 
 const backToDashboard = () => {
   router.push('/dashboard')
+}
+
+const copyCodeService = async (serviceCode: string) => {
+  await navigator.clipboard.writeText(serviceCode)
+  toast('Copied', {
+    description: 'Code already coppied',
+  })
+  modelValue.value.push(serviceCode)
+}
+
+const queryClient = useQueryClient()
+const { mutate, isPending: isSubmitService } = useMutation({
+  mutationFn: ({ data, userID }: { data: { service_code: string[] }; userID: string }) =>
+    postUserServiceData(data, userID),
+  onSuccess: (response) => {
+    if (response) {
+      toast('Success', {
+        description: `${response?.message}`,
+      })
+      queryClient.invalidateQueries({
+        queryKey: ['services', userID],
+      })
+      modelValue.value = []
+    }
+  },
+  onError: (error: Error) => {
+    toast('Error', {
+      description: `Failed to link service: ${error.message}`,
+    })
+  },
+})
+
+const onSubmit = () => {
+  const newData = {
+    service_code: modelValue.value,
+  }
+  mutate({
+    data: newData,
+    userID: userID as string,
+  })
 }
 </script>
